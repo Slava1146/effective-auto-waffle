75fdaf98795c26586a8c6600332118d41bc6cc4d
Viachaslau <chereuhin1146@gmail.com>
2025-06-14 13:07:29 +0300

    Second test, clean up, utils, common functions

--- src/contactList/po/pages/Login.page.ts
+++ src/contactList/po/pages/Login.page.ts
@@ -1,4 +1,4 @@
-import { Locator, Page } from '@playwright/test';
+import { Locator, Page, expect } from '@playwright/test';
 import { BasePage } from './Base.page';
 
 export class LoginPage extends BasePage {
@@ -8,14 +8,29 @@ export class LoginPage extends BasePage {
 
   readonly submitBtn: Locator;
 
+  readonly loginEr: Locator;
+
   constructor(page: Page) {
     super(page);
     this.emailInput = this.page.locator('#email');
     this.passInput = this.page.locator('#password');
     this.submitBtn = this.page.locator('#submit');
+    this.loginEr = this.page.locator('#error');
   }
 
   async goto() {
     await this.page.goto('/');
+  }
+
+  async defaultElVisibilityCheck(): Promise<void> {
+    await expect(this.emailInput).toBeVisible();
+    await expect(this.passInput).toBeVisible();
+    await expect(this.submitBtn).toBeVisible();
+  }
+
+  async login(login: string, password: string): Promise<void> {
+    await this.emailInput.fill(login);
+    await this.passInput.fill(password);
+    await this.submitBtn.click();
   }
 }
